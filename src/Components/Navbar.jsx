98b007ed01b4e1d64f637b0418ca6f2681ab2416
Pakhre Roshan <pakhreroshan@gmail.com>
2025-04-25 21:44:58 +0530

    Added folder structure for navbar and routes

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='container-fluid bg-light p-1'>
+            <div className='container'>
+                navbar
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar.jsx'
+import Footer from './Components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
+    <Footer />
   </StrictMode>,
 )
