562bc356b608eec5bb14619dde7d0f8f36f24530
Pakhre Roshan <pakhreroshan@gmail.com>
2025-04-30 22:39:20 +0530

    feat: initialize Mac page with basic structure and routing setup

--- src/app/Mac/page.js
+++ src/app/Mac/page.js
@@ -1,11 +1,179 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { FreeMode, Pagination } from 'swiper/modules';
+import Link from 'next/link';
 
 const Mac = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className='container-fluid'>
+      <div className='container pb-5'>
+        <Swiper
+          modules={[Pagination, FreeMode]}
+          spaceBetween={15} // Adjusted for better spacing
+          pagination={{ clickable: true }}
+          freeMode={true} // Enables momentum scrolling
+          grabCursor={true} // Shows a hand cursor for better UX
+          className="mySwiper"
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+            },
+            576: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            992: {
+              slidesPerView: 8,
+            },
+            1200: {
+              slidesPerView: 12,
+            },
+          }}
+        >
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/macbookair_light__dfypt7o3xfgy_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>MacBook Air</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/macbook_pro_light__bvnmtmeikj1e_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>MacBook Pro</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/imac_light__cx5ex9nbqxme_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>iMac</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_mini_light__e7ojhup2ezau_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Mac Mini</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_studio_light__fcr3455qk0i2_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Mac Studio</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_pro_light__bly2b0ua4seq_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Mac Pro</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/hmc_light__fq8mh4xb68mm_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Help me choose</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_compare_light__capy8s4wrbhy_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Compare</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/displays_light__d67yrnk0qsa6_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>displays</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_accessories_light__esnwbnk4bxqq_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Accessories</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_os_light__6mb5pqhztcie_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Sequoia</p>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className='col-1' style={{ width: "100%", height: "auto", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link href="/Product" className='text-decoration-none'>
+              <img
+                src="https://www.apple.com/v/mac/home/cg/images/chapternav/mac_shop_light__f0m72gc7jguq_large.svg"
+                className='img-fluid'
+                style={{ borderRadius: "20px", width: "150px", height: "65px", objectFit: "contain" }} // Standardized size
+              />
+              <p className='text-center text-dark'>Shop Mac</p>
+            </Link>
+          </SwiperSlide>
+        </Swiper>
+      </div>
 
-export default Mac
+      <div className='container-fluid px-5'>
+        <video
+          src="/Images/Mac-page-content/banner.mp4"
+          autoPlay
+          muted
+          loop
+          className="w-100"
+          style={{ maxHeight: "800px", objectFit: "cover", borderRadius: "20px" }}
+        >
+          <source src="/Images/Mac-page-content/banner.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default Mac;
