a54210326f089c263d83d83722a988193db7b199
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-04 11:34:15 +0530

    feat(airpods): complete AirPods page with responsive layout and content

--- src/app/AirPods/page.js
+++ src/app/AirPods/page.js
@@ -89,45 +89,25 @@ const Airpods = () => {
         </Swiper>
       </div>
 
-      <div className='container-fluid text-center py-1' style={{ backgroundColor: "#f5f5f7" }}>
-        <p>Find the perfect gift this Mother’s Day.</p>
-      </div>
-
-      <div className="container-fluid px-5 py-5">
-        <div className="row align-items-center">
-          <div className="col-12 col-md-6">
-            <h1 className="display-1 fw-bold text-start">Apple Watch</h1>
+      <div className='container-fluid' style={{ backgroundColor: "#f5f5f7" }}>
+        <Link href="/Product">
+          <div style={{ borderRadius: "20px", objectFit: "cover" }}>
+            <img src="/Images/AirPods-page-image/ss1.png" className='img-fluid w-100 px-5 py-4'></img>
           </div>
-          <div className="col-12 col-md-6 text-md-end text-start mt-3 mt-md-0">
-            <h4 className="fw-bold mb-0">
-              The ultimate device
-              for a healthy life.
-            </h4>
+        </Link>
+        <Link href="/Product">
+          <div style={{ borderRadius: "20px", objectFit: "cover" }}>
+            <img src="/Images/AirPods-page-image/ss2.png" className='img-fluid w-100 px-5 py-4'></img>
           </div>
-        </div>
+        </Link>
+        <Link href="/Product">
+          <div style={{ borderRadius: "20px", objectFit: "cover" }}>
+            <img src="/Images/AirPods-page-image/ss3.png" className='img-fluid w-100 px-5 py-4'></img>
+          </div>
+        </Link>
       </div>
 
-      <div className='container-fluid px-5'>
-        <video
-          src="https://www.apple.com/105/media/us/watch/2024/f0b51c31-e8a5-44d7-b23d-51bd2858454a/anim/hero/xlarge.mp4"
-          autoPlay
-          muted
-          loop
-          className="w-100"
-          style={{ maxHeight: "1000px", objectFit: "cover", borderRadius: "20px" }}
-        >
-          <source src="/Image/Mac-page-content/banner.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-
-      <div className='container-fluid px-5' style={{ paddingTop: "60px", paddingBottom: "60px" }}>
-        <div className="col-12 col-md-6">
-          <h1 className="display-2 fw-semibold text-start">Get to know Apple Watch</h1>
-        </div>
-      </div>
-
-      <div className='px-5 w-100 h-100 mb-5'>
+      <div className='px-5 w-100 h-100 mb-5 mt-5'>
         <Swiper
           modules={[Pagination, FreeMode]}
           spaceBetween={15} // Adjusted for better spacing
@@ -149,28 +129,28 @@ const Airpods = () => {
               slidesPerView: 4,
             },
             1200: {
-              slidesPerView: 5,
+              slidesPerView: 4,
             },
           }}
         >
           <SwiperSlide style={{ width: "100%", height: "auto" }}>
             <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_health__b2yo83wkzoaa_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
+              <img src="https://www.apple.com/v/airpods/x/images/overview/consider/card_noise_cancellation__bcl69t06noci_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
             </Link>
           </SwiperSlide>
           <SwiperSlide style={{ width: "100%", height: "auto" }}>
             <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_fitness__b5owsglf0ieu_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
+              <img src="https://www.apple.com/v/airpods/x/images/overview/consider/card_spatial_audio__uga42js3h4ya_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
             </Link>
           </SwiperSlide>
           <SwiperSlide style={{ width: "100%", height: "auto" }}>
             <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_connectivity__cwtqydvy2laq_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
+              <img src="https://www.apple.com/v/airpods/x/images/overview/consider/card_hearing_health__ss2uxyv3j5m6_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
             </Link>
           </SwiperSlide>
           <SwiperSlide style={{ width: "100%", height: "auto" }}>
             <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_safety__gln97xcew2em_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
+              <img src="https://www.apple.com/v/airpods/x/images/overview/consider/card_iphone__c7gcuw15pxiu_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
             </Link>
           </SwiperSlide>
           <SwiperSlide style={{ width: "100%", height: "auto" }}>
@@ -178,54 +158,17 @@ const Airpods = () => {
               <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_watch_and_iphone__fiq5g9njy5qy_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
             </Link>
           </SwiperSlide>
-          <SwiperSlide style={{ width: "100%", height: "auto" }}>
-            <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_personalization__f8bl9mx85j22_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
-            </Link>
-          </SwiperSlide>
-          <SwiperSlide style={{ width: "100%", height: "auto" }}>
-            <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_adventure__d4xvmn7guk02_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
-            </Link>
-          </SwiperSlide>
-          <SwiperSlide style={{ width: "100%", height: "auto" }}>
-            <Link href="/Product">
-              <img src="https://www.apple.com/v/watch/bq/images/overview/consider/feature_family_setup__dclbe9jpbiie_large_2x.jpg" className='img-fluid' style={{ borderRadius: "20px", width: "500px", height: "800px", objectFit: "cover" }}></img>
-            </Link>
-          </SwiperSlide>
         </Swiper>
       </div>
 
-      <div className='container-fluid px-5 py-5'>
-        <div className="col-12 col-md-6">
-          <h1 className="display-2 fw-semibold py-5">Apple Watch Studio</h1>
-        </div>
-
-        <div className='row py-5'>
-          <div className='col-12'>
-            <Link href="/Product">
-              <img src="/Images/Watch-page -image/Watch studio.png" className='px-0 w-100 img-fluid' style={{ width: "100%", height: "700px", borderRadius: "30px" }}></img>
-            </Link>
+      <div className='container px-5 py-5 w-50'>
+        <div className='row d-flex justify-content-center align-items-center'>
+          <div className='col-12 col-lg-6 display-5 fw-semibold'>
+            <img src="https://static1.squarespace.com/static/64b05fdb50c98c737d01f574/t/66378a4a2de1d2430d44901c/1714916417369/apple-music-logo.png?format=1500w" className='img-fluid' style={{width: "150px", height: "100px", display: "block"}}></img>
+            Get 3 months of Apple Music free with your AirPods.*
           </div>
-        </div>
-      </div>
-
-      <div className='container-fluid px-5 py-5' style={{ backgroundColor: "#ffffff" }}>
-
-        <div className="col-12 col-md-6">
-          <h1 className="display-2 fw-semibold py-5">Apple Watch Essential</h1>
-        </div>
-
-        <div className='row py-5'>
-          <div className='col-12 col-sm-12 col-md-6 col-lg-6'>
-            <Link href="/Product">
-              <img src="/Images/Watch-page -image/ss1.png" className='px-0 w-100 img-fluid' style={{ width: "100%", height: "700px", borderRadius: "30px" }}></img>
-            </Link>
-          </div>
-          <div className='col-12 col-sm-12 col-md-6 col-lg-6'>
-            <Link href="/Product">
-              <img src="/Images//Watch-page -image/ss2.png" className='px-0 w-100 img-fluid' style={{ width: "100%", height: "700px", borderRadius: "30px" }}></img>
-            </Link>
+          <div className='col-12 col-lg-6'>
+            <img src="https://www.apple.com/v/airpods/x/images/overview/music/music_album_stack__bi4gk5n0vwb6_large_2x.png" className='w-100 img-fluid'></img>
           </div>
         </div>
       </div>
